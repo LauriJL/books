89ee3ca9b5187a48f62876805e85f58572b0f5a4
Lauri Leskinen <laurileskinen@icloud.com>
2023-04-26 13:51:19 +0300

    psql set as default database.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,18 +74,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default':
-    # {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': config('PSQL_NAME'),
-    #     'USER': config('PSQL_USER'),
-    #     'PASSWORD': config('PSQL_PASSWORD'),
-    #     'HOST': config('PSQL_HOST'),
-    #     'PORT': config('PSQL_PORT'),
-    # }
     {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('PSQL_NAME'),
+        'USER': config('PSQL_USER'),
+        'PASSWORD': config('PSQL_PASSWORD'),
+        'HOST': config('PSQL_HOST'),
+        'PORT': config('PSQL_PORT'),
     }
+    # {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 REST_FRAMEWORK = {
